7b87ceaa2fa148cf1d77afd25419d6d9bc123d43
Joshua James <josh452063@gmail.com>
2024-04-01 16:04:16 -0500

    Edited Alice for case sensitivity on removing search term from text.

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/Alice.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/Alice.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Alice {
     public static void main(String[] args) {
         String text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’".toLowerCase();
-        String unalteredText = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a term to search for in the first sentence of Alice's Adventures in Wonderland.");
@@ -19,7 +18,7 @@ public class Alice {
             System.out.println("Your search result was found! " +
                     "Your search term first appears at index " + index + ". " +
                     "Your term is " + length + " characters long.");
-            String alteredText = unalteredText.replace(search, "");
+            String alteredText = text.replace(search.toLowerCase(), "");
             System.out.println(alteredText);
         } else {
             System.out.println("No results found");
