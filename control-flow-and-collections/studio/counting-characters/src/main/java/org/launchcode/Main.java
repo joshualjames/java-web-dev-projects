a05d06908f5f49fbbfa13f3e297fbe2052d37c44
Joshua James <josh452063@gmail.com>
2024-04-04 20:44:36 -0500

    control and flow

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/Main.java
@@ -0,0 +1,23 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args){
+    String str = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+    char[] charactersInString = str.toCharArray();
+        HashMap<Character, Integer> countingChars = new HashMap<>();
+
+        for(char letter : charactersInString){
+            if(!countingChars.containsKey(letter)){
+                countingChars.put(letter, 1);
+            }else{
+                countingChars.put(letter, countingChars.get(letter)+1);
+            }
+        }
+        for (Map.Entry<Character, Integer> letters : countingChars.entrySet()){
+            System.out.println(letters.getKey() + ": " + letters.getValue());
+        }
+    }
+}
--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -2,18 +2,19 @@ package org.launchcode;
 
 import java.util.Scanner;
 
+import static java.lang.Double.isNaN;
+
 public class Area {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double r;
+        System.out.print("What is the radius of the circle? Enter a positive number: ");
         do {
-            System.out.print("What is the radius of the circle? Enter a positive number: ");
-            while (!input.hasNextDouble()) {
-                System.out.print("Error. Please enter a positive number: ");
-                input.next();
-            }
             r = input.nextDouble();
+            if (isNaN(r) || r < 0) {
+                System.out.print("Error. Please enter a positive number: ");
+            }
         } while (r < 0);
         double area = Circle.getArea(r);
         System.out.println("The area of the circle is " + area);
